0c3cbb62f34dc7c0126d069ed015d77c089f8d09
Olive <wap3gnh@163.com>
2017-07-19 23:24:13 +0800

    init project

---
+++ src/main/java/com/stone/gesturelock/GestureLockView.java
@@ -0,0 +1,192 @@
+package com.stone.gesturelock;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.support.annotation.IntDef;
+import android.view.View;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Created by Stone on 2017/7/19.
+ */
+
+public class GestureLockView extends View {
+
+    private static final String TAG = "GestureLockView";
+
+    public static final int STATUS_NO_FINGER = 0x00;
+
+    public static final int STATUS_FINGER_ON = 0x01;
+
+    public static final int STATUS_FINGER_UP = 0x02;
+
+    private int mStatus = STATUS_NO_FINGER;
+
+    /**
+     * 宽度
+     */
+    private int mWidth;
+    /**
+     * 高度
+     */
+    private int mHeight;
+    /**
+     * 外圆半径
+     */
+    private int mRadius;
+    /**
+     * 画笔的宽度
+     */
+    private int mStrokeWidth = 2;
+
+    /**
+     * 圆心坐标
+     */
+    private int mCenterX;
+    private int mCenterY;
+    private Paint mPaint;
+
+
+    /**
+     * 箭头（小三角最长边的一半长度 = mArrawRate * mWidth / 2 ）
+     */
+    private float mArrowRate = 0.333f;
+    private int mArrowDegree = -1;
+    private Path mArrowPath;
+    /**
+     * 内圆的半径 = mInnerCircleRadiusRate * mRadus
+     *
+     */
+    private float mInnerCircleRadiusRate = 0.3F;
+
+    /**
+     * 四个颜色，可由用户自定义，初始化时由GestureLockViewGroup传入
+     */
+    private int mColorNoFingerInner;
+    private int mColorNoFingerOutter;
+    private int mColorFingerOn;
+    private int mColorFingerUp;
+
+    @IntDef({STATUS_NO_FINGER, STATUS_FINGER_ON, STATUS_FINGER_UP})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface GestureStatus {}
+
+    public GestureLockView(Context context ,int colorNoFingerInner ,int colorNoFingerOutter ,int colorFingerOn ,int colorFingerUp) {
+        super(context);
+        this.mColorNoFingerInner = colorNoFingerInner;
+        this.mColorNoFingerOutter = colorNoFingerOutter;
+        this.mColorFingerOn = colorFingerOn;
+        this.mColorFingerUp = colorFingerUp;
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);//
+        mArrowPath = new Path();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        mWidth = MeasureSpec.getSize(widthMeasureSpec);
+        mHeight = MeasureSpec.getSize(heightMeasureSpec);
+
+        // 取长和宽中的小值
+        mWidth = mWidth < mHeight ? mWidth : mHeight;
+        mRadius = mCenterX = mCenterY = mWidth / 2;
+        mRadius -= mStrokeWidth / 2;
+
+        // 绘制三角形，初始时是个默认箭头朝上的一个等腰三角形，用户绘制结束后，根据由两个GestureLockView决定需要旋转多少度
+        float arrowLength = mWidth / 2 * mArrowRate;
+        mArrowPath.moveTo(mWidth / 2, mStrokeWidth + 2);
+        mArrowPath.lineTo(mWidth / 2 - arrowLength, mStrokeWidth + 2
+                + arrowLength);
+        mArrowPath.lineTo(mWidth / 2 + arrowLength, mStrokeWidth + 2
+                + arrowLength);
+        mArrowPath.close();
+        mArrowPath.setFillType(Path.FillType.WINDING);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        switch (mStatus) {
+            case STATUS_FINGER_ON:
+
+                // 绘制外圆
+                mPaint.setStyle(Paint.Style.STROKE);
+                mPaint.setColor(mColorFingerOn);
+                mPaint.setStrokeWidth(2);
+                canvas.drawCircle(mCenterX, mCenterY, mRadius, mPaint);
+                // 绘制内圆
+                mPaint.setStyle(Paint.Style.FILL);
+                canvas.drawCircle(mCenterX, mCenterY, mRadius
+                        * mInnerCircleRadiusRate, mPaint);
+                break;
+            case STATUS_FINGER_UP:
+                // 绘制外圆
+                mPaint.setColor(mColorFingerUp);
+                mPaint.setStyle(Paint.Style.STROKE);
+                mPaint.setStrokeWidth(2);
+                canvas.drawCircle(mCenterX, mCenterY, mRadius, mPaint);
+                // 绘制内圆
+                mPaint.setStyle(Paint.Style.FILL);
+                canvas.drawCircle(mCenterX, mCenterY, mRadius
+                        * mInnerCircleRadiusRate, mPaint);
+
+                drawArrow(canvas);
+                break;
+
+            case STATUS_NO_FINGER:
+
+                // 绘制外圆
+                mPaint.setStyle(Paint.Style.FILL);
+                mPaint.setColor(mColorNoFingerOutter);
+                canvas.drawCircle(mCenterX, mCenterY, mRadius, mPaint);
+                // 绘制内圆
+                mPaint.setColor(mColorNoFingerInner);
+                canvas.drawCircle(mCenterX, mCenterY, mRadius
+                        * mInnerCircleRadiusRate, mPaint);
+                break;
+
+        }
+
+
+    }
+
+    /**
+     * 绘制箭头
+     * @param canvas
+     */
+    private void drawArrow(Canvas canvas) {
+        if (mArrowDegree != -1) {
+            mPaint.setStyle(Paint.Style.FILL);
+
+            canvas.save();
+            canvas.rotate(mArrowDegree, mCenterX, mCenterY);
+            canvas.drawPath(mArrowPath, mPaint);
+
+            canvas.restore();
+        }
+    }
+
+    /**
+     * 设置当前模式并重绘界面
+     *
+     * @param status
+     */
+    public void setStatus(@GestureStatus int status)
+    {
+        this.mStatus = status;
+        invalidate();
+    }
+
+    public void setArrowDegree(int degree)
+    {
+        this.mArrowDegree = degree;
+    }
+
+    public int getArrowDegree()
+    {
+        return this.mArrowDegree;
+    }
+}
